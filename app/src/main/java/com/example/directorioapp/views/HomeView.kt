414f0ec31bd6ed9c645e5b28dd514b80d20350a3
Dante Rodriguez <danteyahirrh@gmail.com>
2025-05-29 22:31:39 -0600

    Swipe Eliminar

--- app/src/main/java/com/example/directorioapp/views/HomeView.kt
+++ app/src/main/java/com/example/directorioapp/views/HomeView.kt
@@ -1,3 +1,4 @@
+// HomeView.kt
 package com.example.directorioapp.views
 
 import androidx.compose.foundation.layout.Column
@@ -5,20 +6,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.directorioapp.components.ContactCard
 import com.example.directorioapp.components.FloatButton
 import com.example.directorioapp.components.MainTitle
 import com.example.directorioapp.viewModels.ContactosViewModel
+import me.saket.swipe.SwipeAction
+import me.saket.swipe.SwipeableActionsBox
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,7 +53,20 @@ fun ContentHomeView(it: PaddingValues, navController: NavController, contactosVM
 
         LazyColumn {
             items(contactosList) { contacto ->
-                ContactCard(contacto = contacto)
+                val deleteAction = SwipeAction(
+                    icon = rememberVectorPainter(Icons.Default.Delete),
+                    background = Color.Red,
+                    onSwipe = {
+                        contactosVM.deleteContacto(contacto)
+                    }
+                )
+
+                SwipeableActionsBox(
+                    endActions = listOf(deleteAction),
+                    swipeThreshold = 150.dp // Ajusta si quieres más o menos sensibilidad
+                ) {
+                    ContactCard(contacto = contacto)
+                }
             }
         }
     }
